c5a704140daed0fefdbb18602c716900661be7e7
yCyhVm2bbn38ORd8Bv1Ql06uxssD7MHIUOhQazN <122477802+yCyhVm2bbn38ORd8Bv1Ql06uxssD7MHIUOhQazN@users.noreply.github.com>
2023-01-17 13:07:11 +0100

    Update Installer.lua

--- Installer.lua
+++ Installer.lua
@@ -16,12 +16,10 @@ end
 
 if isfile2("vape/CustomModules/6872274481.lua") then
     writefile("vape/CustomModules/Old6872274481.lua", readfile("vape/CustomModules/6872274481.lua"))
-    writefile("vape/CustomModules/6872274481.lua", [[loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/CustomModules/6872274481.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/yCyhVm2bbn38ORd8Bv1Ql06uxssD7MHIUOhQazN/OhioWare/main/Main.lua"))()]])
+    writefile("vape/CustomModules/6872274481.lua", [[loadstring(game:HttpGet("https://raw.githubusercontent.com/yCyhVm2bbn38ORd8Bv1Ql06uxssD7MHIUOhQazN/OhioWare/main/Main.lua"))()]])
     notify("OhioWare Installed! Old custom You were Using has been Written to vape/CustomModules/Old6872274481.lua")
 else
-    writefile("vape/CustomModules/6872274481.lua", [[loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/CustomModules/6872274481.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/yCyhVm2bbn38ORd8Bv1Ql06uxssD7MHIUOhQazN/OhioWare/main/Main.lua"))()]])
+    writefile("vape/CustomModules/6872274481.lua", [[loadstring(game:HttpGet("https://raw.githubusercontent.com/yCyhVm2bbn38ORd8Bv1Ql06uxssD7MHIUOhQazN/OhioWare/main/Main.lua"))()]])
     if shared.GuiLibrary ~= nil and shared.VapeExecuted then
         notify("OhioWare Installed! Please restart vape for Changes to Load/Save.")
     else
